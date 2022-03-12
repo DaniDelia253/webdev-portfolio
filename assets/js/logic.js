2fcc77e234609fa7bd3ba308a1acd42a42a9c698
DaniDelia253 <90367091+DaniDelia253@users.noreply.github.com>
2022-03-12 12:57:01 -0600

    resume updated

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,4 +1,8 @@
-var offcanvasElementList = [].slice.call(document.querySelectorAll('.offcanvas'))
-var offcanvasList = offcanvasElementList.map(function (offcanvasEl) {
-  return new bootstrap.Offcanvas(offcanvasEl)
+// var offcanvasElementList = [].slice.call(document.querySelectorAll('.offcanvas'))
+// var offcanvasList = offcanvasElementList.map(function (offcanvasEl) {
+//   return new bootstrap.Offcanvas(offcanvasEl)
+// })
+
+var popover = new bootstrap.Popover(document.querySelector('.popover-dismiss'), {
+  trigger: 'focus'
 })
